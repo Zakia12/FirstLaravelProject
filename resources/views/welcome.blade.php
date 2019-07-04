c8dddfea7d1c6b274fbc524e61df445da18f392d
Dina <zr56@njit.edu>
2019-07-03 19:08:47 -0700

    Made changes to welcome page text

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Welcome to the homepage
+                    Welcome to the Zakia's first Laravel HOMEPAGE
                 </div>
 
                 <div class="links">
